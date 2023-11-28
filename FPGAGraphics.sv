b99ce979bd8fe08d52a50a3ea9b0f8b71e6a7c71
soc <bernardocc@al.insper.edu.br>
2023-11-28 17:44:18 -0300

    Preparando VGA

--- FPGAGraphics.sv
+++ FPGAGraphics.sv
@@ -2,6 +2,19 @@
 module FPGAGraphics (
 	input wire logic clk_50,       // input clock (50 MHz)
 	input wire logic rst
+	input  wire logic clk_pix,   // pixel clock
+	input  wire logic rst_pix,   // reset in pixel clock domain
+	output      logic [11:0] sx, // horizontal screen position
+	output      logic [11:0] sy, // vertical screen position
+	output		logic VGA_CLK,
+	output		logic VGA_BLANK_N,
+	output      logic VGA_HS,     // horizontal sync
+	output      logic VGA_VS,     // vertical sync
+	output		logic [7:0] VGA_R,
+	output		logic [7:0] VGA_G,
+	output		logic [7:0] VGA_B,
+	output      logic VGA_SYNC_N,
+	output      logic de         // data enable (low in blanking interval)
 );
 
 
